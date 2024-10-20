b06d5863b9257bedca70249e8ec5d0bf68add10d
TrollRoll2 <patrik.backman@helsinki.fi>
2024-10-20 23:15:29 +0300

    added admin setup

---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO accounts (username, password, role)
+VALUES (
+    'admin1',
+    'scrypt:32768:8:1$Oa1XbfPQDjhV0Lwf$620814a9f53244c044b8fab313120f578e06b9a249b775a848873df4318531de3101395faa4606cd60ab1b558d9905d49306555e406ac8b2b505287e9f40c20a',
+    'admin'
+);
